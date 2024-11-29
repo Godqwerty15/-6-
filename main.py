ed4d23e973e6c3f82254db4a83b4162e202c1221
lambertasdf <140362546+lambertasdf@users.noreply.github.com>
2024-11-29 23:22:13 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 from matplotlib import rc
 rc('font', family='Malgun Gothic')
 plt.rcParams['axes.unicode_minus'] = False
+name = input("표준점수 분포를 볼 과목을 선택하세요 : ")
 data = pd.read_csv('20231231.CSV', encoding = 'EUC-KR')
-korean_data = data[data['영역'] == '국어']
-module_a.Graph_Drawing(korean_data['표준점수'],korean_data['남자'],korean_data['여자'])
+korean_data = data[data['유형'] == name]
+module_a.Graph_Drawing(korean_data['표준점수'],korean_data['남자'],korean_data['여자'],name)
