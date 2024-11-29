8f722b7e45cecbbabdf230c2153013ea1b4ef2a2
Godqwerty15 <hytn0929@gmail.com>
2024-11-29 23:22:34 +0900

    Update module_a.py

--- module_a.py
+++ module_a.py
@@ -1,11 +1,10 @@
 import matplotlib.pyplot as plt
 
-#그래프 출력 함수
-def Graph_Drawing(x_values,male_scores,female_scores):
+def Graph_Drawing(x_values,male_scores,female_scores,name):
     plt.figure(figsize=(10, 6))
     plt.plot(x_values, male_scores, label='남자')
     plt.plot(x_values, female_scores, label='여자')
-    plt.title('남자와 여자 국어 성적의 표준점수 분포')
+    plt.title('남자와 여자 %s 성적의 표준점수 분포'%name)
     plt.xlabel('표준점수')
     plt.ylabel('학생 수')
     plt.legend()
