0c2786c3d50faae9bd72744a0365016f09f4ff0a
Godqwerty15 <hytn0929@gmail.com>
2024-11-29 23:24:47 +0900

    Update module_a.py

--- module_a.py
+++ module_a.py
@@ -4,7 +4,7 @@ def Graph_Drawing(x_values,male_scores,female_scores,name):
     plt.figure(figsize=(10, 6))
     plt.plot(x_values, male_scores, label='남자')
     plt.plot(x_values, female_scores, label='여자')
-    plt.title('남자와 여자 %s 성적의 표준점수 분포'%name)
+    plt.title('2024학년도 수능 %s 과목분포'%name)
     plt.xlabel('표준점수')
     plt.ylabel('학생 수')
     plt.legend()
