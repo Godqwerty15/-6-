91f6f5d9c982a382b7e58bcedf26ad26f544d8fa
Godqwerty15 <hytn0929@gmail.com>
2024-11-29 23:38:00 +0900

    Update module_a.py

--- module_a.py
+++ module_a.py
@@ -2,8 +2,8 @@ import matplotlib.pyplot as plt
 
 def Graph_Drawing(x_values,male_scores,female_scores,name):
     plt.figure(figsize=(10, 6))
-    plt.plot(x_values, male_scores, label='남자')
-    plt.plot(x_values, female_scores, label='여자')
+    plt.plot(x_values, male_scorers, label='남자')
+    plt.plot(x_values, female_scorers, label='여자')
     plt.title('2024학년도 수능 %s 과목분포'%name)
     plt.xlabel('표준점수')
     plt.ylabel('학생 수')
