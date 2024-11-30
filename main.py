0bae758e7379f9599e9e9c2ff40a7042c017cc2b
Godqwerty15 <hytn0929@gmail.com>
2024-11-30 20:56:21 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,12 @@ import matplotlib.pyplot as plt
 from matplotlib import rc
 rc('font', family='Malgun Gothic')
 plt.rcParams['axes.unicode_minus'] = False
-name = input("표준점수 분포를 볼 과목을 선택하세요 : ")
 data = pd.read_csv('20231231.CSV', encoding = 'EUC-KR')
+subjects = data['유형']
+unique_subjects = subjects.unique().tolist()
+print("표준점수를 볼 수 있는 과목 목록 : ",end='')
+print(unique_subjects)
+name = input("표준점수 분포를 볼 과목을 선택하세요 : ")
 korean_data = data[data['유형'] == name]
 module_a.Graph_Drawing(korean_data['표준점수'],korean_data['남자'],korean_data['여자'],name)
+print(korean_data['남자'])
